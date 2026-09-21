7bce9f94ab571d8acb11c0a0432a4ccc4d1afce0
Deepika malviya <deepika.malviya@nexturn.com>
2026-09-21 13:52:43 +0530

    Add project files.

---
+++ CSharp- Basics/Comments/CommentDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp__Basics.Comments
+{
+    internal class CommentDemo
+    {
+
+        // This is a single-line comment
+
+        /* This is a 
+           multi-line comment */
+
+        /// <summary>
+        /// XML documentation comment used to generate documentation
+        /// </summary>
+        public void DocumentedMethod() { }
+    }
+}
